9a3ca64239321444f7dc0128c8444f3c1d142700
Kinarki <nickfriend81@gmail.com>
2015-12-17 13:12:39 -0800

    registration call complete

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,6 @@
 app.controller('loginCtrl', function(loginData){
     var login = this;
 
-    //login.test = function() {
-    //    $('.left-div').addClass('left');
-    //    $('.right-div').addClass('right');
-    //    $('.move, .under-tree ').addClass('right-transition');
-    //    setTimeout(function(){
-    //        $('.login-form, .sign-up').addClass('hidden');
-    //    },650);
-    //};
-
     login.signOut = function() {
         $('.left-div').removeClass('left');
         $('.right-div').removeClass('right');
@@ -57,11 +48,11 @@ app.factory("loginData", function($http){
     return loginService;
 });
 
-app.controller("modalCtrl", function(registerData, $log){
+app.controller("modalCtrl", function(registerData) {
     var modal = this;
 
-    modal.userRegister = function(rEm,rPa, rPi){
-        registerData.regData(rEm,rPa, rPi).then(function(response){
+    modal.userRegister = function(regName, regEmail,regPass, regPic, $log){
+        registerData.regData(regName, regEmail,regPass, regPic).then(function(response){
             if(response.data.success) {
                 $('.left-div').addClass('left');
                 $('.right-div').addClass('right');
@@ -73,7 +64,7 @@ app.controller("modalCtrl", function(registerData, $log){
             }
             else{
                 alert('Error: ' + response.data.errors);
-                $log.info(response);
+                $log.info(response.data.errors);
             }
         })
     };
@@ -82,12 +73,12 @@ app.controller("modalCtrl", function(registerData, $log){
 app.service("registerData", function($http,$log){
     var reg = {};
 
-    reg.regData = function(regEmail, regPass, regPic){
-        var userRegData = $.param({email: regEmail, password: regPass, profilePicture: regPic});
-        $log.info(regEmail, regPass, regPic);
+    reg.regData = function(regName, regEmail, regPass, regPic){
+        var userRegData = $.param({display_name: regName, email: regEmail, password: regPass, profilePicture: regPic});
+        $log.info(regName, regEmail, regPass, regPic);
 
-        $http({
-            url: "php",
+        return $http({
+            url: "php/register_user.php",
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
