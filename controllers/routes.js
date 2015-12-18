5da94b57460159c6d5ab136c67e4e641bf3ea941
adiaguidry <adiaguidry@gmail.com>
2015-12-17 16:33:09 -0800

    api debug

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,43 +1,52 @@
-app.controller('routeCtrl', function($routeProvider){
+app.controller('routeCtrl', function ($routeProvider) {
 
 })
-.config(function($routeProvider){
-    $routeProvider
-        .when('/blog',{
-            templateUrl: 'pages/blog.html',
-            controller: 'blogCtrl'
-        })
-        .when('/profile',{
-            templateUrl: 'pages/profile.html',
-            controller: 'profileCtrl'
-        })
-        .when('/create',{
-            templateUrl: 'pages/create.html',
-            controller: 'createCtrl'
-        });
-    //    .when('/',{
-    //        templateUrl: 'pages/lander.html'
-    //    }
-    //);
-});
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/blog', {
+                templateUrl: 'pages/blog.html',
+                controller: 'blogCtrl'
+            })
+            .when('/profile', {
+                templateUrl: 'pages/profile.html',
+                controller: 'profileCtrl'
+            })
+            .when('/create', {
+                templateUrl: 'pages/create.html',
+                controller: 'createCtrl'
+            });
+        //    .when('/',{
+        //        templateUrl: 'pages/lander.html'
+        //    }
+        //);
+    });
 
-app.controller('blogCtrl', function(getData, $log){
+app.controller('blogCtrl', function (getData, $log) {
     var blog = this;
-    blog.array =[{title: 'Apples', summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."},
-        {title: 'Oranges', summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."},
-        {title: 'Bananas', summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."},];
-    blog.info ={};
+    blog.array = [{
+        title: 'Apples',
+        summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."
+    },
+        {
+            title: 'Oranges',
+            summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."
+        },
+        {
+            title: 'Bananas',
+            summary: "Lorem Ipsum is simply dummy of the printing and typesetting text of is simply dummy text of the printing and typesetting industry."
+        },];
+    blog.info = {};
     getData.callData()
-        .then(function(response){
+        .then(function (response) {
             blog.info = response.data.data;
             blog.array.push(blog.info);
-        }, function(response){
+        }, function (response) {
             $log.info(response);
         });
 });
-app.controller('createCtrl', function($http, $log){
+app.controller('createCtrl', function ($http, $log) {
     var blog = this;
-    blog.creatBlog = function(){
+    blog.creatBlog = function () {
         var enrty = $.param({
             title: 'title',
             summary: 'summary'
@@ -47,35 +56,35 @@ app.controller('createCtrl', function($http, $log){
         url: 'http://s-apis.learningfuze.com/blog/create.json',
         header: {'Content-Type': 'application/x-www-form-urlencoded'},
         method: 'POST'
-    }).success(function(response){
+    }).success(function (response) {
         $log.info('success in create: ', response);
-    }).error(function(){
+    }).error(function () {
         $log.error('error');
     })
 });
-app.controller('profileCtrl', function($http, $log){
-   var pro = this;
+app.controller('profileCtrl', function ($http, $log, getData) {
+    var pro = this;
     pro.edit = true;
-    $http({
-        url: 'php/get_profile.php',
-        header: {'Content-Type': 'application/x-www-form-urlencoded'},
-        method: 'POST',
-        data:{uid:5}
-    }).success(function(response){
-        $log.info('success for profile; ', response);
-        pro.info = response.data;
-    }).error(function(){
-        $log.error('error' + response);
-    });
+
+    pro.profileData = function () {
+        getData.callData()
+            .then(function (response) {
+                $log.info("the profile: ", response);
+            }, function (response) {
+                $log.info(response);
+            });
+    };
 
 });
-app.factory("getData", function($http){
+
+app.factory("getData", function ($http) {
     var service = {};
-    var url = "http://s-apis.learningfuze.com/blog/list.json";
-    service.callData = function(){
+    var url = "php/get_profile.php";
+    service.callData = function () {
         return $http({
             url: url,
             method: 'POST',
+            data:{uid: 5},
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
@@ -85,6 +94,6 @@ app.factory("getData", function($http){
     return service;
 });
 
-app.controller("logoutCtrl", function(){});
+
 
 
